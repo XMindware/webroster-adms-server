c205dd65d481d1e6f7b9e290d1b1276b46aea132
Xavier Alfeiran <xavieralfeiran@Xaviers-MacBook-Pro.local>
2024-11-13 14:00:10 -0500

    error 404 report

--- app/Http/Controllers/iclockController.php
+++ app/Http/Controllers/iclockController.php
@@ -47,7 +47,7 @@ class iclockController extends Controller
             "TransTimes=00:00;14:05\r\n" .
             "TransInterval=1\r\n" .
             "TransFlag=1111000000\r\n" .
-//            "TimeZone=-6\r\n" .
+            "TimeZone=-6\r\n" .
             "Realtime=1\r\n" .
             "Encrypt=0";
 
