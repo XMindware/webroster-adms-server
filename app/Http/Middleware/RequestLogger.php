bf425abeda1b395d07aa819ded570b4f8f682c9e
Xavier Alfeiran <xavieralfeiran@Xaviers-MacBook-Pro.local>
2024-11-13 14:24:00 -0500

    only log the routes of sync

--- app/Http/Middleware/RequestLogger.php
+++ app/Http/Middleware/RequestLogger.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Log;
 
 class RequestLogger
 {
+
     /**
      * Handle an incoming request.
      *
@@ -17,23 +18,31 @@ class RequestLogger
      */
     public function handle(Request $request, Closure $next)
     {
-        // Log the incoming request
-        Log::channel('request_log')->info('Request Logged', [
-            'method' => $request->getMethod(),
-            'url' => $request->fullUrl(),
-            'headers' => $request->headers->all(),
-            'body' => $request->all(),
-        ]);
+        $routesToLog = [
+            '/iclock/cdata',
+            '/iclock/getrequest'
+        ];
 
-        // Process the request and get the response
-        $response = $next($request);
+        if (in_array($request->path(), $routesToLog)) {
+            
+            // Log the incoming request
+            Log::channel('request_log')->info('Request Logged', [
+                'method' => $request->getMethod(),
+                'url' => $request->fullUrl(),
+                'headers' => $request->headers->all(),
+                'body' => $request->all(),
+            ]);
 
-        // Log the response
-        Log::channel('request_log')->info('Response Logged', [
-            'status' => $response->status(),
-            'content' => $response->getContent(),
-        ]);
+            // Process the request and get the response
+            $response = $next($request);
 
-        return $response;
+            // Log the response
+            Log::channel('request_log')->info('Response Logged', [
+                'status' => $response->status(),
+                'content' => $response->getContent(),
+            ]);
+
+            return $response;
+        }
     }
 }
