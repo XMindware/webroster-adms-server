5e2c15a7b7f2608296bf087aa5248327409825ee
Xavier Alfeiran <xavieralfeiran@Xaviers-MacBook-Pro.local>
2024-11-21 12:35:39 -0500

    update executed_at

--- app/Http/Controllers/iclockController.php
+++ app/Http/Controllers/iclockController.php
@@ -146,6 +146,8 @@ class iclockController extends Controller
         $response = '';
         foreach ($commands as $command) {
             $response .= $command->data . "\r\n";
+            $command->update(['executed_at' => now()]);
+            $command->save();         
         }
         Log::info('getrequest Response', ['response' => $response]);
         return $response;
